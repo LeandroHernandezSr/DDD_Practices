4922017d2b8f64f03ff91708bdd78a150e180fb3
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-11-07 07:44:23 -0300

    Add: se procede a crear la capa de infraestructura

--- rental/src/main/java/com/ddd/bicycle/rental/application/RentalApplication.java
+++ rental/src/main/java/com/ddd/bicycle/rental/application/RentalApplication.java
@@ -1,4 +1,4 @@
-package com.ddd.bicycle.rental;
+package com.ddd.bicycle.rental.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
