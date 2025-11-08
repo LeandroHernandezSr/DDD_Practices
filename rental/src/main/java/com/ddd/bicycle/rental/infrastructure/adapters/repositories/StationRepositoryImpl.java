d5eb665c8c499647ef3c35eb4a1a5c9c19764044
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-11-08 01:34:39 -0300

    Add: se avanza a crear mapper

--- rental/src/main/java/com/ddd/bicycle/rental/infrastructure/adapters/repositories/StationRepositoryImpl.java
+++ rental/src/main/java/com/ddd/bicycle/rental/infrastructure/adapters/repositories/StationRepositoryImpl.java
@@ -3,10 +3,19 @@ package com.ddd.bicycle.rental.infrastructure.adapters.repositories;
 import com.ddd.bicycle.rental.domain.model.StationId;
 import com.ddd.bicycle.rental.domain.model.station.Station;
 import com.ddd.bicycle.rental.domain.repository.StationRepository;
+import com.ddd.bicycle.rental.infrastructure.adapters.repositories.jpa.StationJpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class StationRepositoryImpl implements StationRepository {
+
+    private final StationJpaRepository repository;
+
+    public StationRepositoryImpl(StationJpaRepository repository){
+        this.repository=repository;
+    }
+
     @Override
     public Station findById(StationId stationId) {
         return null;
