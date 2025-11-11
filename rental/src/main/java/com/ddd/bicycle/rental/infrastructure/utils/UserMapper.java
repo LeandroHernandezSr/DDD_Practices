5c70fd518f334a243797b5fb8cda81812b18eb03
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-11-11 15:19:04 -0300

    Add: se trabaja en el mapper de usuario

--- rental/src/main/java/com/ddd/bicycle/rental/infrastructure/utils/UserMapper.java
+++ rental/src/main/java/com/ddd/bicycle/rental/infrastructure/utils/UserMapper.java
@@ -1,8 +1,40 @@
 package com.ddd.bicycle.rental.infrastructure.utils;
 
+import com.ddd.bicycle.rental.infrastructure.adapters.entities.StationEntity;
+import com.ddd.bicycle.rental.infrastructure.adapters.entities.UserEntity;
+import com.ddd.bicycle.rental.infrastructure.entrypoints.dtos.UserDto;
 import org.springframework.stereotype.Component;
 
 @Component
 public class UserMapper {
 
+    public UserEntity toEntity(UserDto userDto) {
+        UserEntity userEntity = new UserEntity();
+
+        if (userDto != null) {
+            userEntity.setId(userDto.getId());
+        }
+
+        userEntity.setUser(userDto.getUser());
+        userEntity.setActive(userDto.getActive());
+
+        if (userDto.getStationId() != null) {
+            StationEntity  stationEntity = new StationEntity();
+            stationEntity.setId(userDto.getStationId());
+            userEntity.setStation(stationEntity);
+        }else{
+            userEntity.setStation(null);
+        }
+
+        return userEntity;
+    }
+
+    public UserDto toDto(UserEntity userEntity) {
+        return new UserDto.Builder()
+                .id(userEntity.getId())
+                .user(userEntity.getUser())
+                .active(userEntity.getActive())
+                .stationId(userEntity.getStation().getId())
+                .build();
+    }
 }
