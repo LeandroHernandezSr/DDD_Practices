0b34cc80fb1c2d3489d6700390346d684cbb8b1b
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-11-19 09:58:53 -0300

    Add: se procede a crear servicio para orquestacion de casos de uso

---
+++ rental/src/main/java/com/ddd/bicycle/rental/domain/model/services/RentBikeService.java
@@ -0,0 +1,14 @@
+package com.ddd.bicycle.rental.domain.model.services;
+
+import java.util.Map;
+
+import com.ddd.bicycle.rental.domain.model.station.Station;
+import com.ddd.bicycle.rental.domain.model.user.User;
+
+public class RentBikeService {
+    public Map<String,Object> rent(Station station,User user){
+        
+        return null;
+    }
+
+}
