50dfd810101496dbc1e0d664f79f2714f5d02f40
Leandro Hernandez <l.hernandez071999@gmail.com>
2025-11-19 10:06:08 -0300

    Add: Se crea servicio para rentar bicis

--- rental/src/main/java/com/ddd/bicycle/rental/domain/model/services/RentBikeService.java
+++ rental/src/main/java/com/ddd/bicycle/rental/domain/model/services/RentBikeService.java
@@ -1,14 +1,28 @@
 package com.ddd.bicycle.rental.domain.model.services;
 
+import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.stereotype.Service;
+
 import com.ddd.bicycle.rental.domain.model.station.Station;
+import com.ddd.bicycle.rental.domain.model.user.Bike;
 import com.ddd.bicycle.rental.domain.model.user.User;
 
+@Service
 public class RentBikeService {
     public Map<String,Object> rent(Station station,User user){
-        
-        return null;
-    }
 
+        Map<String,Object>aggregations=new HashMap<>();
+
+        if (user != null && user.getActive()){
+           user.takeBike(new Bike(station.getStationId()));
+           station.getTotalCapacity().aumentBikeCount();
+
+           aggregations.put("user", user);
+           aggregations.put("station", station);
+        }
+
+        return aggregations;
+    }
 }
